54eacd806e3f9509e18278a8ea166ef503b67f6c
Stephen Tinubu <60358867+Pro-tonn@users.noreply.github.com>
2021-05-26 21:34:01 -0500

    updated snippets

--- assets/Open CV.py
+++ assets/Open CV.py
@@ -4,6 +4,13 @@ import cv2
 #Color value 0 =  Grayscale images
 #Color value 1 = Alpha value most likely with transparent backgrounds
 img = cv2.imread('assets\logo.jpg', -1)
+img = cv2.resize(img, (400, 400))
+
+#rotational snippet
+#img = cv2.rotate(img, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+#saving a processed image (including the file format of the image)
+#cv2.imwrite('New_img.jpg', img)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
